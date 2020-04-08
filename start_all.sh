41d98e882057002dea8c19d35d6debdca33fd790
andrii482 <61418776+andrii482@users.noreply.github.com>
2020-04-08 11:57:23 +0300

    Update start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -1,3 +1,5 @@
+```bash
+
 docker stop redis ethereum-testnet bob-node charlie-node  bob-eth bob-xrp charlie-xrp david-eth david-ltc david-node felix-ltc felix-xtz felix-node  hugo-xtz hugo-xmr hugo-node ingrid-xmr ingrid-node ubuntu_truffle
 docker rm redis ethereum-testnet bob-node charlie-node  bob-eth bob-xrp charlie-xrp david-eth david-ltc david-node felix-ltc felix-xtz felix-node  hugo-xtz hugo-xmr hugo-node ingrid-xmr ingrid-node ubuntu_truffle
 docker network rm local-ilp
@@ -73,7 +75,7 @@ docker run -d \
   --asset_scale 1 \
   --settlement_api_bind_address 0.0.0.0:3000
 sleep 2
- docker run -d \
+docker run -d \
   --name david-eth \
   --network local-ilp \
   -e "RUST_LOG=interledger=trace" \
@@ -217,6 +219,169 @@ sleep 2
 git clone https://github.com/andrii482/7nodes.git
 cd 7nodes
 docker build --no-cache -t ubuntu_truffle .
-cd
 sleep 2
 docker run --rm -itd  --name ubuntu_truffle  --network local-ilp  ubuntu_truffle
+cd
+sleep 2
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://ingrid-node:7770 accounts create ingrid \
+  --auth hi_ingrid \
+  --ilp-address example.ingrid \
+  --asset-code XMR \
+  --asset-scale 1 \
+  --ilp-over-http-incoming-token ingrid_password
+sleep 2
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://ingrid-node:7770 accounts create hugo \
+  --auth hi_ingrid \
+  --ilp-address example.hugo \
+  --asset-code XMR \
+  --asset-scale 1 \
+  --settlement-engine-url http://ingrid-xmr:3000 \
+  --ilp-over-http-incoming-token hugo_password \
+  --ilp-over-http-outgoing-token ingrid_password \
+  --ilp-over-http-url http://hugo-node:7770/accounts/ingrid/ilp \
+  --settle-threshold 2 \
+  --settle-to 1 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://hugo-node:7770 accounts create ingrid \
+  --auth hi_hugo \
+  --ilp-address example.ingrid \
+  --asset-code XMR \
+  --asset-scale 2 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://hugo-xmr:3000 \
+  --ilp-over-http-incoming-token ingrid_password \
+  --ilp-over-http-outgoing-token hugo_password \
+  --ilp-over-http-url http://ingrid-node:7770/accounts/hugo/ilp \
+  --settle-threshold 2 \
+  --settle-to 1 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://hugo-node:7770 accounts create felix \
+  --auth hi_hugo \
+  --ilp-address example.felix \
+  --asset-code XTZ \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://hugo-xtz:3000 \
+  --ilp-over-http-incoming-token felix_password \
+  --ilp-over-http-outgoing-token hugo_other_password \
+  --ilp-over-http-url http://felix-node:7770/accounts/hugo/ilp \
+  --settle-threshold 2 \
+  --settle-to 1 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://felix-node:7770 accounts create hugo \
+  --auth hi_felix \
+  --ilp-address example.hugo \
+  --asset-code XTZ \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://felix-xtz:3000 \
+  --ilp-over-http-incoming-token hugo_other_password \
+  --ilp-over-http-outgoing-token felix_password \
+  --ilp-over-http-url http://hugo-node:7770/accounts/felix/ilp \
+  --settle-threshold 2 \
+  --settle-to 1 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://felix-node:7770 accounts create david \
+  --auth hi_felix \
+  --ilp-address example.david \
+  --asset-code LTC \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://felix-ltc:3000 \
+  --ilp-over-http-incoming-token david_password \
+  --ilp-over-http-outgoing-token felix_other_password \
+  --ilp-over-http-url http://david-node:7770/accounts/felix/ilp \
+  --settle-threshold 2 \
+  --settle-to 1 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://david-node:7770 accounts create felix \
+  --auth hi_david \
+  --ilp-address example.felix \
+  --asset-code LTC \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://david-ltc:3000 \
+  --ilp-over-http-incoming-token felix_other_password \
+  --ilp-over-http-outgoing-token david_password \
+  --ilp-over-http-url http://felix-node:7770/accounts/david/ilp \
+  --settle-threshold 2 \
+  --settle-to 1 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2  
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://bob-node:7770 accounts create david \
+  --auth hi_bob \
+  --ilp-address example.david \
+  --asset-code ETH \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://bob-eth:3000 \
+  --ilp-over-http-incoming-token david_other_password \
+  --ilp-over-http-outgoing-token bob_password \
+  --ilp-over-http-url http://david-node:7770/accounts/bob/ilp \
+  --settle-threshold 20 \
+  --settle-to 10 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2  
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://david-node:7770 accounts create bob \
+  --auth hi_david \
+  --ilp-address example.bob \
+  --asset-code ETH \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://david-eth:3000 \
+  --ilp-over-http-incoming-token bob_password \
+  --ilp-over-http-outgoing-token david_other_password \
+  --ilp-over-http-url http://bob-node:7770/accounts/david/ilp \
+  --settle-threshold 2 \
+  --settle-to 1 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2  
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://bob-node:7770 accounts create charlie \
+  --auth hi_bob \
+  --ilp-address example.charlie \
+  --asset-code XRP \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://bob-xrp:3001 \
+  --ilp-over-http-incoming-token charlie_password \
+  --ilp-over-http-outgoing-token bob_other_password \
+  --ilp-over-http-url http://charlie-node:7770/accounts/bob/ilp \
+  --settle-threshold 2200 \
+  --settle-to 100 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2  
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://charlie-node:7770 accounts create bob \
+  --auth hi_charlie \
+  --ilp-address example.bob \
+  --asset-code XRP \
+  --asset-scale 1 \
+  --max-packet-amount 100000 \
+  --settlement-engine-url http://charlie-xrp:3000 \
+  --ilp-over-http-incoming-token bob_other_password \
+  --ilp-over-http-outgoing-token charlie_password \
+  --ilp-over-http-url http://bob-node:7770/accounts/charlie/ilp \
+  --settle-threshold 2200 \
+  --settle-to 100 \
+  --min-balance -150000 \
+  --routing-relation Peer
+sleep 2  
+docker run --rm --network local-ilp interledgerrs/ilp-cli --node http://charlie-node:7770 accounts create charlie \
+  --auth hi_charlie \
+  --ilp-address example.charlie \
+  --asset-code XRP \
+  --asset-scale 1 \
+  --ilp-over-http-incoming-token charlie_password
